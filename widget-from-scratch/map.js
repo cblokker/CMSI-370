a3858a523f75d850769a710d7ef26639c30946c3
Chase Blokker <chaseblokker@yahoo.com>
2012-12-02 19:45:40 -0800

    Cleaned up code a bit, added rotation when user clicks and drags.  Rotation ends when mouseup event occurs.The rotation lacks fluidity b/c the arrow part of the image is always pointing in direction of cursor coordinate.

--- widget-from-scratch/map.js
+++ widget-from-scratch/map.js
@@ -1,21 +1,38 @@
 var startGender = function () {
+    
+    var imageCenterX,
+        imageCenterY,
+        mouseX,
+        mouseY,
+        radians,
+        degree;
 
+    var img = $('.image');
+    var offset = img.offset();
+    var mousedown = false;
 
-var img = $('.image');
-var offset = img.offset();
-
-    $('#image').mousedown(function(evt) {
-    	var center_x = (offset.left) + (img.width()/2);
-        var center_y = (offset.top) + (img.height()/2);
-        var mouse_x = evt.pageX; var mouse_y = evt.pageY;
-        var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
-        var degree = (radians * (180 / Math.PI) * -1) + 90; 
-        img.css('-moz-transform', 'rotate('+degree+'deg)');
-        img.css('-webkit-transform', 'rotate('+degree+'deg)');
-        img.css('-o-transform', 'rotate('+degree+'deg)');
-        img.css('-ms-transform', 'rotate('+degree+'deg)');
+    $('.image').mousedown(function(e){
+    	mousedown = true;
+    	e.preventDefault();
     });
 
-    $(document).mousemove(mouse);
+    $(document).mouseup(function(){
+        mousedown = false;
+    })
+    
+    $('.image').mousemove(function(e) {
+    	if (mousedown == false) return;
+
+    	center_x = (offset.left) + (img.width()/2);
+        center_y = (offset.top) + (img.height()/2);
+        mouse_x = e.pageX;
+        mouse_y = e.pageY;
+        radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+        degree = (radians * (180 / Math.PI) * -1) + 135;
+        img.css('-moz-transform', 'rotate(' + degree + 'deg)');
+        img.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+        img.css('-o-transform', 'rotate(' + degree + 'deg)');
+        img.css('-ms-transform', 'rotate(' + degree + 'deg)');
+    });
 }
 

bbe78c52ef7b8d0a6adbbff2ea6e4512080c057d
Chase Blokker <chaseblokker@yahoo.com>
2012-12-02 22:39:41 -0800

    created variable opacity of image as a function of degree of rotation.  Overlayed venus sign over mars sign.  Plan to fade in venus sign while the mars sign fades out and visa versa.

--- widget-from-scratch/map.js
+++ widget-from-scratch/map.js
@@ -11,6 +11,8 @@ var startGender = function () {
     var offset = img.offset();
     var mousedown = false;
 
+    $(".image").css("opacity", "100");
+
     $('.image').mousedown(function(e){
     	mousedown = true;
     	e.preventDefault();
@@ -23,16 +25,22 @@ var startGender = function () {
     $('.image').mousemove(function(e) {
     	if (mousedown == false) return;
 
-    	center_x = (offset.left) + (img.width()/2);
-        center_y = (offset.top) + (img.height()/2);
-        mouse_x = e.pageX;
-        mouse_y = e.pageY;
-        radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+    	imageCenterX = (offset.left) + (img.width()/2);
+        imageCenterY = (offset.top) + (img.height()/2);
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+        radians = Math.atan2(mouseX - imageCenterX, mouseY - imageCenterY);
         degree = (radians * (180 / Math.PI) * -1) + 135;
         img.css('-moz-transform', 'rotate(' + degree + 'deg)');
         img.css('-webkit-transform', 'rotate(' + degree + 'deg)');
         img.css('-o-transform', 'rotate(' + degree + 'deg)');
         img.css('-ms-transform', 'rotate(' + degree + 'deg)');
+        //$(this).fadeTo("slow", 0.33);
+
+        opacity = ((135 - degree)*0.008);      
+        if (mouseX > imageCenterX) {
+            $(".image").css("opacity", opacity);
+        }
     });
 }
 
